ee06a07f999400d49e3ac77eeb40fa171e6d6eaa
何力 <heli@21tb.com>
2014-12-10 00:19:04 +0800

    domain类添加

---
+++ src/main/java/com/yuanhe/eighth/domain/annotation/BaseDomain.java
@@ -0,0 +1,7 @@
+package com.yuanhe.eighth.domain.annotation;
+
+/**
+ * Created by dam on 2014/6/30.
+ */
+public class BaseDomain implements java.io.Serializable {
+}
---
+++ src/main/java/com/yuanhe/eighth/domain/annotation/Column.java
@@ -0,0 +1,19 @@
+package com.yuanhe.eighth.domain.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by dam on 2014/7/14.
+ */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Column {
+    String name();
+    boolean pk()default false;
+    int length() default 5;
+    int decimal()default 0;
+    String comment()default "";
+}
